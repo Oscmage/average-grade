2d625bf493dd1afcedf5cae87bda771be2a3d07c
Oscar Evertsson <oscar.evertsson@live.com>
2017-10-31 16:21:22 +0100

    Updated version.

--- myscript.js
+++ myscript.js
@@ -1,57 +1,122 @@
 var bigTable = document.getElementsByClassName("table-responsive");
 
-var children = bigTable[0].children;
-console.log(children);
-var h2Text = [];
+var tables = document.getElementsByClassName("table-bordered");
 
-for (var i = 0; i < children.length; i = i + 2) {
-  //Take only the h2s
-  h2Text.push(children[i].outerText);
-}
+var addCheckboxesForProgram = () => {
+  for (let i = 0; i < tables.length; i++) {
+    const trArray = tables[i].children[0].children[0];
+    const sp2 = trArray.children[0];
 
-var CHANGETHISVARIABLE = "TKITE";
-var programs = [];
-programs.push(CHANGETHISVARIABLE);
+    const sp1 = document.createElement("input");
+    sp1.type = "checkbox";
+    sp1.checked = true;
+    sp1.class = "id";
 
-var correctTableAtIndex = -1;
-var correctTableAtIndexes = [];
+    // Get a reference to the parent element
+    const parentTr = sp2.parentNode;
 
-//Add the indexes for the different tables to the correctTableAtIndexes variable.
-for (var j = 0; j < h2Text.length; j++) {
-  for (var i = 0; i < programs.length; i++) {
-    if (h2Text[j].includes(programs[i])) {
-      correctTableAtIndex = j;
-      correctTableAtIndexes.push(j);
+    // Insert the new element into the DOM before sp2
+    parentTr.insertBefore(sp1, sp2);
+
+    sp1.addEventListener("click", evt => {
+      const toElement = evt.toElement;
+      const relatedCheckboxes = tables[i].children[1].getElementsByTagName(
+        "input"
+      );
+
+      for (let j = 0; j < relatedCheckboxes.length; j++) {
+        relatedCheckboxes[j].checked = toElement.checked;
+      }
+    });
+  }
+};
+
+addCheckboxesForProgram();
+
+function addCheckboxesForCourses() {
+  for (var i = 0; i < tables.length; i++) {
+    var trArray = tables[i].children[1].children;
+    for (var j = 0; j < trArray.length; j++) {
+      var tr = trArray[j];
+      var trChildren = tr.children;
+
+      var sp1 = document.createElement("input");
+      sp1.type = "checkbox";
+      sp1.checked = true;
+      sp1.class = "id";
+
+      // Get a reference to the element, before we want to insert the element
+      var sp2 = trChildren[0];
+      // Get a reference to the parent element
+      var parentTr = sp2.parentNode;
+
+      // Insert the new element into the DOM before sp2
+      parentTr.insertBefore(sp1, sp2);
     }
   }
 }
 
-var tot = 0;
-var courses = new Array();
-var totalCredits = 0;
-
-// Loop through the seperate tables to get all courses for calculation.
-for (var x = 0; x < correctTableAtIndexes.length; x++) {
-  var courses = [].concat.apply(
-    courses,
-    children[2 * correctTableAtIndexes[x] + 1]
-      .getElementsByTagName("tbody")[0]
-      .getElementsByTagName("tr")
-  );
-}
-
-console.log(courses);
-// Within each table loop through the courses and calculate the total grade times points.
-for (var i = 0; i < courses.length; i++) {
-  var course = courses[i].cells;
-  if (course[3].outerText === "G") {
-    courses.splice(i, 1); // Remove the elements, it should not affect the whole calculation.
-  } else {
-    var grade = parseFloat(course[3].outerText.replace(",", "."));
-    var credits = parseFloat(course[2].outerText.replace(",", "."));
-    totalCredits = totalCredits + credits;
-    tot = tot + grade * credits;
+function getAllCourses() {
+  var allCourses = new Array();
+  // Loop through the seperate tables to get all allCourses for calculation.
+  for (var x = 0; x < tables.length; x++) {
+    var allCourses = [].concat.apply(
+      allCourses,
+      tables[x].getElementsByTagName("tbody")[0].getElementsByTagName("tr")
+    );
   }
+  return allCourses;
 }
 
-console.log("Your average grade is: " + tot / totalCredits);
+function getCheckedCourses() {
+  var allCourses = getAllCourses();
+
+  // Within each table loop through the allCourses and calculate the total grade times points.
+  for (var i = 0; i < allCourses.length; i++) {
+    var inputField = allCourses[i].getElementsByTagName("input")[0];
+    var course = allCourses[i].cells;
+
+    if (inputField.checked === false) {
+      allCourses.splice(i, 1); // Remove the elements, it should not affect the whole calculation.
+    }
+  }
+
+  return allCourses;
+}
+
+function getAverageGrade() {
+  var tot = 0;
+  var totalCredits = 0;
+
+  var checkedCourses = getCheckedCourses();
+
+  for (var i = 0; i < checkedCourses.length; i++) {
+    var course = checkedCourses[i].cells;
+    if (course[3].outerText === "G") {
+      checkedCourses.splice(i, 1); // Remove the elements, it should not affect the whole calculation.
+    } else {
+      var grade = parseFloat(course[3].outerText.replace(",", "."));
+      var credits = parseFloat(course[2].outerText.replace(",", "."));
+
+      totalCredits = totalCredits + credits;
+      tot = tot + grade * credits;
+    }
+  }
+  return tot / totalCredits;
+}
+
+function init() {
+  addCheckboxesForCourses();
+}
+
+init();
+
+console.log("Your average grade is: " + getAverageGrade());
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  switch (message.type) {
+    case "getAverageGrade":
+      sendResponse(getAverageGrade());
+      break;
+  }
+});
--- popup.js
+++ popup.js
@@ -1,32 +1,21 @@
 document.addEventListener(
   "DOMContentLoaded",
   function() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { greeting: "hello" }, function(
-        response
-      ) {
-        if (response) {
-          console.log("Already there");
-        } else {
-          console.log("Not there, inject contentscript");
-        }
+    var button = document.getElementById("calculate-grade-button");
+    button.addEventListener("click", function() {
+      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        chrome.tabs.sendMessage(
+          tabs[0].id,
+          {
+            type: "getAverageGrade"
+          },
+          function(response) {
+            document.getElementById("grade").textContent =
+              "Your average grade is: " + response;
+          }
+        );
       });
     });
-    console.log("WHOHO!");
-    var calculateGradeButton = document.getElementById(
-      "calculate-grade-button"
-    );
-    const children = document.getElementsByClassName("table-responsive");
-    calculateGradeButton.addEventListener(
-      "click",
-      function() {
-        const inputValue = document.getElementById("program").value;
-        console.log(inputValue);
-        const children = document.getElementsByClassName("table-responsive");
-        console.log(children);
-      },
-      false
-    );
   },
   false
 );
