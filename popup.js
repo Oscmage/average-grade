46670e255fc5b4b10265161497d8b1a4531de8eb
Oscar Evertsson <oscare@spotify.com>
2018-07-10 23:24:24 +0200

    Add more detailed description and link to starting page.

--- popup.js
+++ popup.js
@@ -1,19 +1,26 @@
 document.addEventListener(
   "DOMContentLoaded",
-  function () {
+  function() {
     var button = document.getElementById("calculate-grade-button");
-    button.addEventListener("click", function () {
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    button.addEventListener("click", function() {
+      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         chrome.tabs.sendMessage(
           tabs[0].id,
           {
             type: "getAverageGrade"
           },
-          function (response) {
+          function(response) {
             document.getElementById("grade").textContent =
               "Your average grade is: " + response.average;
           }
         );
+      });
+    });
+
+    const link = document.getElementById("start-page-link");
+    link.addEventListener("click", function() {
+      chrome.tabs.update({
+        url: "https://www.student.ladok.se/student/#/avslutade"
       });
     });
   },
